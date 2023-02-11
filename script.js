79f73ab4ad23577e92259125b5a08f27055e2246
akhil <86508192+akhildgithub@users.noreply.github.com>
2023-02-11 22:39:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
+edit.addEventListener("click", (e) => {
+  e.preventDefault()
+  let k = key1.value
+  let v = value1.value
+  let break1 = 0
+  for (var i = 0; i < localStorage.length; i++) {
+    if (k == localStorage.key(i)) {
+      localStorage.setItem(k, v)
+      break1++;
+      break;
+    }
+  }
+  if(k=='' || v=='')
+    alert('Not provided required values')
+  else{
+  if (break1 == 0) {
+    alert('There is no such key')
+  }
+  }
+  todos.innerHTML = items();
+})
+
+add.addEventListener("click", (e) => {
+  e.preventDefault()
+  let k = key1.value
+  let v = value1.value
+  let breaking = 0
+  for (var i = 0; i < localStorage.length; i++) {
+    if (k == localStorage.key(i)) {
+      alert('This key already exists')
+      breaking++;
+      break;
+    }
+  }
+  if(k=='' || v=='')
+    alert('Not provided required values')
+  else{ 
+    if (breaking == 0)
+    localStorage.setItem(k, v)
+  }
+  todos.innerHTML = items();
+})
+
+delet.addEventListener("click", (e) => {
+  e.preventDefault()
+  let k = key1.value
+  if (localStorage.length == 0)
+    alert('It\'s already empty')
+  for (var i = 0; i < localStorage.length; i++) {
+    if (k == localStorage.key(i)) {
+      localStorage.removeItem(k);
+      break;
+    }
+    if (i == localStorage.length - 1) {
+      alert('There is no such key')
+    }
+  }
+  todos.innerHTML = items();
+})
+
+clear1.addEventListener("click", (e) => {
+  e.preventDefault()
+  if (localStorage.length == 0)
+    alert('It\'s already empty')
+  else
+    localStorage.clear();
+  todos.innerHTML = items();
+})
+
 function items() {
   var text = 'Todos are '
   for (var i = 0; i < localStorage.length; i++) {
@@ -10,58 +79,3 @@ function items() {
 }
 
 todos.innerHTML = items();
-
-setTimeout(() => {
-  let change = prompt('1:edit item \n2:add item \n3:delete item \n4:remove all items')
-  let k, v;
-  switch (change) {
-    case '1':
-      k = prompt('enter the key')
-      v = prompt('change the value ')
-      let break1 = 0
-      for (var i = 0; i < localStorage.length; i++) {
-        if (k == localStorage.key(i)) {
-          localStorage.setItem(k, v)
-          break1++;
-          break;
-        }
-      }
-      if (break1 == 0) {
-        alert('there is no such key')
-      }
-      break;
-    case '2':
-      k = prompt('enter the key')
-      v = prompt('add the value ')
-      let breaking = 0
-      for (var i = 0; i < localStorage.length; i++) {
-        if (k == localStorage.key(i)) {
-          alert('this key already exists')
-          breaking++;
-          break;
-        }
-      }
-      if (breaking == 0)
-        localStorage.setItem(k, v)
-      break;
-    case '3':
-      k = prompt('enter the key')
-      for (var i = 0; i < localStorage.length; i++) {
-        if (k == localStorage.key(i)) {
-          localStorage.removeItem(k);
-          break;
-        }
-        if (i == localStorage.length - 1) {
-          alert('there is no such key')
-        }
-      }
-      break;
-    case '4':
-      if (localStorage.length == 0)
-        alert('it\'s already empty')
-      else
-        localStorage.clear()
-      break;
-  }
-  todos.innerHTML = items();
-}, 3000)
